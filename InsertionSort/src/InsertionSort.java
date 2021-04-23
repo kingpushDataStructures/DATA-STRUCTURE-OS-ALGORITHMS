2a9a20f9bb86e021dc225adf85592022b752bb73
kingpushDataStructures <80451696+kingpushDataStructures@users.noreply.github.com>
2021-04-23 08:27:32 +0000

    Add files via upload

---
+++ InsertionSort/src/InsertionSort.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+
+/**
+ * Insertion-sort of an array of characters into nondecreasing order
+ */
+public class InsertionSort {
+
+    public static void main(String[] args) {
+
+        char[] letters = {'c', 'a', 'j', 'q', 'r', 's', 'z', 'v'};
+
+        char[] others = new char[8];
+        Arrays.fill(others, 'a');
+
+        System.out.println(others);
+        System.out.println(letters);
+//        insertionSort(letters);
+        Arrays.sort(letters);
+        System.out.println(letters);
+    }
+
+    public static void insertionSort(char[] data) {
+        int n = data.length;
+        for (int k = 1; k < n; k++) {  // begin with second character
+
+            char cur = data[k];  // time to insert cur = data[k]
+
+            int j = k;  // find correct index j for cur
+
+            while (j > 0 && data[j - 1] > cur) {  // thus, data[j-1] must go after cur
+                data[j] = data[j - 1];  // slide data[j-1] rightward
+                j--;  // and consider previous j for cur
+            }
+            data[j] = cur;  // this is the proper place for cur
+        }
+    }
+}
